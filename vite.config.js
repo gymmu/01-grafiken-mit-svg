e267b8c128fa803caa1f32dfdf2b0eeb10af45ed
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-07-17 12:29:24 +0000

    Add prettier format.
    Habe neue optionen zu prettier hinzugefügt, so das auch SVG formatiert werden.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import FullReload from "vite-plugin-full-reload";
+import { defineConfig } from 'vite';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  plugins: [FullReload(["images/**/*.svg"])],
-  server: {
-    host: "0.0.0.0",
-  },
-  build: {
-    outDir: "docs",
-  },
-  base: "/01-intro-websites-cedricgeissmann/",
+    plugins: [FullReload(['images/**/*.svg'])],
+    server: {
+        host: '0.0.0.0'
+    },
+    build: {
+        outDir: 'docs'
+    },
+    base: '/01-intro-websites-cedricgeissmann/'
 });
