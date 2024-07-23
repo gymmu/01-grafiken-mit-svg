ddc783b0d632bec97f08bc2c39af4d1ce29b5e1e
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-07-23 05:49:40 +0000

    Add new tests.

--- tests/svg.test.jsx
+++ tests/svg.test.jsx
@@ -1,16 +1,36 @@
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, beforeAll, beforeEach } from 'vitest'
 import { render } from '@testing-library/react'
 import Aufgabe01 from '../images/aufgabe-01.svg?react'
 
-describe('SVG', () => {
-    test('new svg render method', () => {
-        const { container } = render(<Aufgabe01 />)
+class SVG {
+    #body
 
-        expect(container).toBeDefined()
+    constructor(component) {
+        this.#body = render(component).container
+    }
 
-        const c = container.querySelector('#circle')
-        expect(c).toBeDefined()
-        expect(c.getAttribute('x')).toBe('0')
-        expect(c.getAttribute('y')).toBe('0')
+    get body() {
+        return this.#body
+    }
+}
+
+describe('Aufgabe 01', () => {
+    const svg = new SVG(<Aufgabe01 />)
+
+    test('Das SVG zur Aufgabe 01 konnte erstellt werden.', () => {
+        expect(svg).toBeDefined()
+    })
+
+    test('Es gibt ein "<circle>" Element', () => {
+        const circle = svg.body.querySelector('circle')
+        console.log(circle)
+        expect(circle).toBeDefined()
+    })
+
+    test('Das "<circle>" Element hat den Mittelpunkt (250, 250)', () => {
+        const circle = svg.querySelector('circle')
+        console.log(svg.innerHTML)
+        expect(circle.getAttribute('cx')).toBe(250)
+        expect(circle.getAttribute('cy')).toBe(250)
     })
 })
